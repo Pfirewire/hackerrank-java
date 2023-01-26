eb97d7ca07ccc54aa531267669707f74fa69e7c8
Stephen Pflug <stephen@pflug.us>
2023-01-26 13:33:14 -0600

    completed question successfully

--- src/Solution.java
+++ src/Solution.java
@@ -1,8 +1,15 @@
 import java.util.Scanner;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
 //        Scanner scan = new Scanner(System.in);
 //        int i = scan.nextInt();
@@ -17,14 +24,23 @@ public class Solution {
 //        System.out.println("Double: " + d);
 //        System.out.println("Int: " + i);
 
-        Scanner sc=new Scanner(System.in);
-        System.out.println("================================");
-        for(int i=0;i<3;i++){
-            String s1=sc.next();
-            int x=sc.nextInt();
-            //Complete this line
-            System.out.printf("%-15s%03d%n", s1, x);
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("================================");
+//        for(int i=0;i<3;i++){
+//            String s1=sc.next();
+//            int x=sc.nextInt();
+//            //Complete this line
+//            System.out.printf("%-15s%03d%n", s1, x);
+//        }
+//        System.out.println("================================");
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        for(int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d%n", N, i, (N*i));
         }
-        System.out.println("================================");
+
+        bufferedReader.close();
     }
 }

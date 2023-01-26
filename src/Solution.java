bc511bc9af70c5b1c0e0f53b421030c7a3cdaf7e
Stephen Pflug <stephen@pflug.us>
2023-01-26 13:52:12 -0600

    completed question successfully

--- src/Solution.java
+++ src/Solution.java
@@ -43,22 +43,45 @@ public class Solution {
 //
 //        bufferedReader.close();
 
-        Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=0;i<t;i++){
-            int a = in.nextInt();
-            int b = in.nextInt();
-            int n = in.nextInt();
-            int total = a;
-            for(int j = 0; j < n; j++) {
-                total += (Math.pow(2, j)) * b;
-                if(j == n - 1) {
-                    System.out.println(total);
-                } else {
-                    System.out.printf("%d ", total);
-                }
+//        Scanner in = new Scanner(System.in);
+//        int t=in.nextInt();
+//        for(int i=0;i<t;i++){
+//            int a = in.nextInt();
+//            int b = in.nextInt();
+//            int n = in.nextInt();
+//            int total = a;
+//            for(int j = 0; j < n; j++) {
+//                total += (Math.pow(2, j)) * b;
+//                if(j == n - 1) {
+//                    System.out.println(total);
+//                } else {
+//                    System.out.printf("%d ", total);
+//                }
+//            }
+//        }
+//        in.close();
+
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++)
+        {
+
+            try
+            {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)System.out.println("* byte");
+                if(x >= -32768 && x <= 32767) System.out.println("* short");
+                if(x >= Math.pow(-2, 31) && x <= (Math.pow(2, 31) -1)) System.out.println("* int");
+                if(x >= Math.pow(-2, 63) && x <= (Math.pow(2, 63) -1)) System.out.println("* long");
+                //Complete the code
             }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+
         }
-        in.close();
     }
 }

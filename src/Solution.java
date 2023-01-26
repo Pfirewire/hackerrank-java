fd3ddeb26d4477374f7fad6a0057c251c30ab1f5
Stephen Pflug <stephen@pflug.us>
2023-01-26 13:28:39 -0600

    completed question successfully

--- src/Solution.java
+++ src/Solution.java
@@ -4,17 +4,27 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        int i = scan.nextInt();
-        double d = scan.nextDouble();
-        scan.nextLine();
-        String s = scan.nextLine();
+//        Scanner scan = new Scanner(System.in);
+//        int i = scan.nextInt();
+//        double d = scan.nextDouble();
+//        scan.nextLine();
+//        String s = scan.nextLine();
+//
+//        scan.close();
+//        // Write your code here.
+//
+//        System.out.println("String: " + s);
+//        System.out.println("Double: " + d);
+//        System.out.println("Int: " + i);
 
-        scan.close();
-        // Write your code here.
-
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String s1=sc.next();
+            int x=sc.nextInt();
+            //Complete this line
+            System.out.printf("%-15s%03d%n", s1, x);
+        }
+        System.out.println("================================");
     }
 }

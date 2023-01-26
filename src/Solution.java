6858555205441352b6770466663ae06260e3ee69
Stephen Pflug <stephen@pflug.us>
2023-01-26 13:56:12 -0600

    completed question successfully

--- src/Solution.java
+++ src/Solution.java
@@ -61,27 +61,35 @@ public class Solution {
 //        }
 //        in.close();
 
-        Scanner sc = new Scanner(System.in);
-        int t=sc.nextInt();
+//        Scanner sc = new Scanner(System.in);
+//        int t=sc.nextInt();
+//
+//        for(int i=0;i<t;i++)
+//        {
+//
+//            try
+//            {
+//                long x=sc.nextLong();
+//                System.out.println(x+" can be fitted in:");
+//                if(x>=-128 && x<=127)System.out.println("* byte");
+//                if(x >= -32768 && x <= 32767) System.out.println("* short");
+//                if(x >= Math.pow(-2, 31) && x <= (Math.pow(2, 31) -1)) System.out.println("* int");
+//                if(x >= Math.pow(-2, 63) && x <= (Math.pow(2, 63) -1)) System.out.println("* long");
+//                //Complete the code
+//            }
+//            catch(Exception e)
+//            {
+//                System.out.println(sc.next()+" can't be fitted anywhere.");
+//            }
+//
+//        }
 
-        for(int i=0;i<t;i++)
-        {
-
-            try
-            {
-                long x=sc.nextLong();
-                System.out.println(x+" can be fitted in:");
-                if(x>=-128 && x<=127)System.out.println("* byte");
-                if(x >= -32768 && x <= 32767) System.out.println("* short");
-                if(x >= Math.pow(-2, 31) && x <= (Math.pow(2, 31) -1)) System.out.println("* int");
-                if(x >= Math.pow(-2, 63) && x <= (Math.pow(2, 63) -1)) System.out.println("* long");
-                //Complete the code
-            }
-            catch(Exception e)
-            {
-                System.out.println(sc.next()+" can't be fitted anywhere.");
-            }
-
-        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int lineNumber = 1;
+        do {
+            String line = scan.nextLine();
+            System.out.printf("%d %s%n", lineNumber++, line);
+        } while(scan.hasNext());
     }
 }

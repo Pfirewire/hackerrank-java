cb4d85c2e98c5cb66a6ec51d7821eaeff058e3be
Stephen Pflug <stephen@pflug.us>
2023-01-26 14:30:53 -0600

    completed question successfully

--- src/Solution.java
+++ src/Solution.java
@@ -6,7 +6,51 @@ import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
-t java.security.*;
+import java.security.*;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        String dayOfWeek = "";
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+        switch (calendar.get(Calendar.DAY_OF_WEEK)) {
+            case 1:
+                dayOfWeek = "SUNDAY";
+                break;
+            case 2:
+                dayOfWeek = "MONDAY";
+                break;
+            case 3:
+                dayOfWeek = "TUESDAY";
+                break;
+            case 4:
+                dayOfWeek = "WEDNESDAY";
+                break;
+            case 5:
+                dayOfWeek = "THURSDAY";
+                break;
+            case 6:
+                dayOfWeek = "FRIDAY";
+                break;
+            case 7:
+                dayOfWeek = "SATURDAY";
+                break;
+        }
+        return dayOfWeek;
+    }
+
+}
 
 public class Solution {
 
@@ -22,6 +66,8 @@ public class Solution {
 //            System.out.println("java.lang.Exception: Breadth and height must be positive");
 //        }
 //    }
+
+
 
     public static void main(String[] args) throws IOException {
 
@@ -111,45 +157,64 @@ public class Solution {
 //            System.out.print(area);
 //        }
 
-        DoNotTerminate.forbidExit();
+//        DoNotTerminate.forbidExit();
+//
+//        try {
+//            Scanner in = new Scanner(System.in);
+//            int n = in.nextInt();
+//            in.close();
+//            //String s=???; Complete this line below
+//            String s = String.valueOf(n);
+//            //Write your code here
+//
+//
+//            if (n == Integer.parseInt(s)) {
+//                System.out.println("Good job");
+//            } else {
+//                System.out.println("Wrong answer.");
+//            }
+//        } catch (DoNotTerminate.ExitTrappedException e) {
+//            System.out.println("Unsuccessful Termination!!");
+//        }
 
-        try {
-            Scanner in = new Scanner(System.in);
-            int n = in.nextInt();
-            in.close();
-            //String s=???; Complete this line below
-            String s = String.valueOf(n);
-            //Write your code here
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
-            if (n == Integer.parseInt(s)) {
-                System.out.println("Good job");
-            } else {
-                System.out.println("Wrong answer.");
-            }
-        } catch (DoNotTerminate.ExitTrappedException e) {
-            System.out.println("Unsuccessful Termination!!");
-        }
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 }
 
 //The following class will prevent you from terminating the code using exit(0)!
-class DoNotTerminate {
-
-    public static class ExitTrappedException extends SecurityException {
-
-        private static final long serialVersionUID = 1;
-    }
-
-    public static void forbidExit() {
-        final SecurityManager securityManager = new SecurityManager() {
-            @Override
-            public void checkPermission(Permission permission) {
-                if (permission.getName().contains("exitVM")) {
-                    throw new ExitTrappedException();
-                }
-            }
-        };
-        System.setSecurityManager(securityManager);
-    }
-}
+//class DoNotTerminate {
+//
+//    public static class ExitTrappedException extends SecurityException {
+//
+//        private static final long serialVersionUID = 1;
+//    }
+//
+//    public static void forbidExit() {
+//        final SecurityManager securityManager = new SecurityManager() {
+//            @Override
+//            public void checkPermission(Permission permission) {
+//                if (permission.getName().contains("exitVM")) {
+//                    throw new ExitTrappedException();
+//                }
+//            }
+//        };
+//        System.setSecurityManager(securityManager);
+//    }
+//}

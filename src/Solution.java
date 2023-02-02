ab9b8d825d9bdd467a897f7af214171b24078f05
Stephen Pflug <stephen@pflug.us>
2023-02-02 11:48:05 -0600

    question completed successfully

--- src/Solution.java
+++ src/Solution.java
@@ -317,16 +317,22 @@ public class Solution {
 //            System.out.println("Unsuccessful Termination!!");
 //        }
 
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        String B=sc.next();
-        System.out.println(A.length() + B.length());
-        if(A.compareTo(B) > 0) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
-        System.out.printf("%s %s", A.substring(0,1).toUpperCase() + A.substring(1), B.substring(0,1).toUpperCase() + B.substring(1));
+//        Scanner sc=new Scanner(System.in);
+//        String A=sc.next();
+//        String B=sc.next();
+//        System.out.println(A.length() + B.length());
+//        if(A.compareTo(B) > 0) {
+//            System.out.println("Yes");
+//        } else {
+//            System.out.println("No");
+//        }
+//        System.out.printf("%s %s", A.substring(0,1).toUpperCase() + A.substring(1), B.substring(0,1).toUpperCase() + B.substring(1));
+
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start, end));
 
     }
 //    static class Inner{
